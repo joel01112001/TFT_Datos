6cb3226569be4d251eb194346bf9d737d0ccc9e3
joel01112001 <159768246+joel01112001@users.noreply.github.com>
2025-07-10 15:08:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from video_writer import annotate_video
 from config import use_model
 
 def main():
-    input_video = "prueba8.mp4"
-    output_video = "BLIP_Soccernet_SNGS-186.mp4"
+    input_video = "prueba.mp4"
+    output_video = "resultado.mp4"
     dorsals_confirmados, tracker_info, player_colors = analyze_video(input_video, use_model)
     annotate_video(input_video, output_video, dorsals_confirmados, player_colors)
 
